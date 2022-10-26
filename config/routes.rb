4f823173c4668f56799e5617c48dfb56ecfd9f1f
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2022-10-25 21:56:44 -0300

    Adicionado estilo a algumas páginas

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   namespace :admins_backoffice do
-    get 'welcome/index'
-  end
+    end
   namespace :users_backoffice do
-    get 'welcome/index'
-  end
+    end
   devise_for :admins
   resources :types
   resources :bagmons
   devise_for :users
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
